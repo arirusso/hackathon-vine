6d2ed52b1b9fb52a1ce8e27c59e40b3359e8370d
Ari Russo <ari.russo@gmail.com>
2013-04-27 15:57:25 -0400

    stuff

--- server.rb
+++ server.rb
@@ -1,6 +1,7 @@
 require "data_mapper"
 require "json"
 require "sinatra"
+require "uri"
 
 #set :port, 8000
 
@@ -20,6 +21,7 @@ DataMapper.finalize
 #@connection.set_cookie_store('/tmp/cookie.dat')
 
 def form
+  hashtags = Hashtag.all.map(&:name).map { |n| "<li>#{n}</li>" }.join
   '<form name="input" action="/" method="post">
    <h1>Music Hackathon</h1>
    <h3>Enter a hashtag</h3> <br /><input type="text" name="hashtag">
@@ -27,14 +29,14 @@ def form
    </form>
    <h4>Hashtags so far:</h4>
    <ul>
-  ' + Hashtag.all.map(&:name).map { |n| "<li>#{n}</li>" }.join +
+  ' + hashtags +
   '</ul>'
 end
 
 #def vidurl
 #  h = Hashtag.last
-#  q = URI::encode(h.name)
-#  Connection.instance.post("http://vineviewer.co/actions/search.php?q=blah%2Fv%2F+&rpp=100&page=1", params)
+#  q = URI.escape(h.name)
+#  #  Connection.instance.post("http://vineviewer.co/actions/search.php?q=blah%2Fv%2F+&rpp=100&page=1", params)
 #  
 #end
 
@@ -52,3 +54,4 @@ end
 #  content_type :json
 #  { :key1 => 'value1', :key2 => 'value2' }.to_json
 #end
+
