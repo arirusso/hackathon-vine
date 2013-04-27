02ad79388261a990f7e4a0485e7218b1cfa3048f
Ari Russo <ari.russo@gmail.com>
2013-04-27 15:48:32 -0400

    first

---
+++ hashtag.rb
@@ -0,0 +1,54 @@
+require "data_mapper"
+require "json"
+require "sinatra"
+
+set :port, 8000
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+class Hashtag
+  include DataMapper::Resource
+  property :id,        Serial  
+  property :name,         String, :required => true
+  property :submitted_at, DateTime
+end
+DataMapper.finalize
+
+#Hashtag.auto_migrate!
+
+#@connection = HTTPClient.new
+#@connection.set_cookie_store('/tmp/cookie.dat')
+
+def form
+  '<form name="input" action="/" method="post">
+   <h1>Music Hackathon</h1>
+   <h3>Enter a hashtag</h3> <br /><input type="text" name="hashtag">
+   <input type="submit" value="Submit">
+   </form>
+   <h4>Hashtags so far:</h4>
+   <ul>
+  ' + Hashtag.all.map(&:name).map { |n| "<li>#{n}</li>" }.join +
+  '</ul>'
+end
+
+#def vidurl
+#  h = Hashtag.last
+#  q = URI::encode(h.name)
+#  Connection.instance.post("http://vineviewer.co/actions/search.php?q=blah%2Fv%2F+&rpp=100&page=1", params)
+#  
+#end
+
+get '/' do
+  form
+end
+
+post '/' do
+  Hashtag.create(:name => params[:hashtag], :submitted_at => Time.now).save
+  form
+end
+
+#post '/video' do
+#  vidurl
+#  content_type :json
+#  { :key1 => 'value1', :key2 => 'value2' }.to_json
+#end
